da641640887edb8bd3a2ae00c832b15ef05033d1
ksanax <163411035+astnq@users.noreply.github.com>
2024-11-11 18:22:00 +0700

    Add files via upload

---
+++ src/queue.cpp
@@ -0,0 +1,47 @@
+#include "../include/queue.h"
+
+Queue::Queue(int cap) : capacity(cap), front(0), rear(-1), size(0){
+    data = new string[capacity];
+}
+
+Queue::Queue() : capacity(30), front(0), rear(-1), size(0) {
+    data = new string[capacity]; //Размер списка
+}
+
+Queue::~Queue() {
+    delete[] data;
+}
+
+bool Queue::isEmpty(){
+    return size == 0;
+}
+
+void Queue::push(string value) {
+    if (size == capacity) {
+        throw overflow_error("Очередь переполнена "); //Проверка на переполнение 
+    }
+    rear = (rear + 1) % capacity; //Циклическое увеличение индекса 
+    data[rear] = value; // Добавление элемента 
+    size++; // Увеличение размера очереди 
+}
+
+string Queue::pop() { 
+    if (size == 0) { 
+        throw overflow_error("Очередь пуста ");
+    }
+    string value = data[front]; // Сохранение значение для возврата 
+    front = (front + 1) % capacity; //Циклическое увеличение индекса 
+    size--; // Уменьшение размера очереди 
+    return value; // Возврат удаленного элемента 
+}
+
+string Queue::peek() {
+    if (isEmpty()) {
+        throw underflow_error("Очередь пуста "); //Проверка на переполнение 
+    }
+    return data[front];
+}
+
+int Queue::Size() {
+    return size;
+}
