25456b7d005984a3a939ddf8ce0b725181b22e12
ksanax <163411035+astnq@users.noreply.github.com>
2025-10-08 23:35:14 -0400

    Update queue.cpp

--- src/queue.cpp
+++ src/queue.cpp
@@ -1,47 +1,47 @@
 #include "../include/queue.h"
 
-Queue::Queue(int cap) : capacity(cap), front(0), rear(-1), size(0){
+Queue::Queue(int cap) : capacity(cap), front(0), rear(-1), size(0) { 
+    data = new string[capacity];    
+}
+
+Queue::Queue() : capacity(30), front(0), rear(-1), size(0) { 
     data = new string[capacity];
 }
 
-Queue::Queue() : capacity(30), front(0), rear(-1), size(0) {
-    data = new string[capacity]; //Размер списка
-}
-
-Queue::~Queue() {
+Queue::~Queue() { 
     delete[] data;
 }
 
-bool Queue::isEmpty(){
+bool Queue::isEmpty() { 
     return size == 0;
 }
 
-void Queue::push(string value) {
-    if (size == capacity) {
-        throw overflow_error("Очередь переполнена "); //Проверка на переполнение 
+void Queue::push(string value) {  
+    if (size == capacity) { 
+        throw overflow_error(" Queue is full ");  
     }
-    rear = (rear + 1) % capacity; //Циклическое увеличение индекса 
-    data[rear] = value; // Добавление элемента 
-    size++; // Увеличение размера очереди 
+    rear = (rear + 1) % capacity;
+    data[rear] = value;
+    size++;
 }
 
 string Queue::pop() { 
     if (size == 0) { 
-        throw overflow_error("Очередь пуста ");
+        throw overflow_error(" Queue is empty");  
     }
-    string value = data[front]; // Сохранение значение для возврата 
-    front = (front + 1) % capacity; //Циклическое увеличение индекса 
-    size--; // Уменьшение размера очереди 
-    return value; // Возврат удаленного элемента 
-}
+    string value = data[front];
+    front = (front + 1) % capacity;
+    size--;
+    return value;
+} 
 
-string Queue::peek() {
-    if (isEmpty()) {
-        throw underflow_error("Очередь пуста "); //Проверка на переполнение 
+string Queue::peek() { 
+    if (isEmpty()) { 
+        throw underflow_error(" Queue is empty ");  
     }
     return data[front];
 }
 
-int Queue::Size() {
+int Queue::Size() { 
     return size;
 }
