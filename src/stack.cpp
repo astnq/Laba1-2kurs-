2e7429cae1e16655b41a64514ffaaae377a58d29
ksanax <163411035+astnq@users.noreply.github.com>
2025-10-08 23:35:39 -0400

    Update stack.cpp

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,10 +1,10 @@
 #include "../include/stack.h"
 
-Stack::Stack(size_t size) : capacity(size), head(-1) {
+Stack::Stack(size_t size) : capacity(size), head(-1) { 
     data = new string[capacity];
 }
 
-Stack::Stack() : capacity(30), head(-1) { //Устанавливаем начальную емкость 10
+Stack::Stack() : capacity(30), head(-1) { 
     data = new string[capacity];
 }
 
@@ -12,31 +12,31 @@ Stack::~Stack() {
     delete[] data;
 }
 
-void Stack::push(string value) {
+void Stack::push(string value) { 
     if (head == capacity - 1) { 
-        throw overflow_error("Стек переполнен ");
+        throw overflow_error(" Stack is full"); 
     }
-    data[++head] = value; //Добавляем элемент и увелечения индекса 
+    data[++head] = value;
 }
 
-string Stack::pop() {
-    if (head == - 1) { 
-        throw underflow_error("Стек пустой ");
+string Stack::pop() { 
+    if (head == -1) { 
+        throw underflow_error(" Stack is empty"); 
     }
-    return data[head--]; //Возврат элемента и уменьшение индекса 
+    return data[head--];
 }
 
-string Stack::peek() {
-    if (head == - 1) {
-        throw underflow_error("Стек пустой ");
-    }
+string Stack::peek() { 
+    if (head == -1) { 
+        throw underflow_error(" Stack is empty");  
+    } 
     return data[head];
 }
 
-bool Stack::isEmpty(){
-    return head == - 1;
+bool Stack::isEmpty() {  
+    return head == -1;
 }
 
-size_t Stack::size() {
+size_t Stack::size() { 
     return head + 1;
 }
